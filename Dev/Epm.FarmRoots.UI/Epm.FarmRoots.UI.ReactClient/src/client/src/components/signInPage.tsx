76bfe30d0cfa567dd0245a95f185cf64a3b3ee96
venakatasai002 <venkatasai_sathvik@epam.com>
2024-09-19 14:35:11 +0530

    new changes for delete and update files

--- Dev/Epm.FarmRoots.UI/Epm.FarmRoots.UI.ReactClient/src/client/src/components/signInPage.tsx
+++ Dev/Epm.FarmRoots.UI/Epm.FarmRoots.UI.ReactClient/src/client/src/components/signInPage.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import SignIn from './SignIn';
-import Header from './Header';
-import Footer from './Footer';
 
 const SignInPage: React.FC = () => {
         return (
                 <>
-                        <Header />
                         <div className="flex justify-center items-center min-h-screen">
                                 <SignIn />
                         </div>
-                        <Footer />
                 </>
         );
 };
