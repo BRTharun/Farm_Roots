82f23e5c9f40477365b7adc2bd2d99e4e85b74c6
tarun_kumar <tarun_kumar@epam.com>
2024-10-07 15:49:20 +0530

    final

--- Dev/Epm.FarmRoots.UI/Epm.FarmRoots.UI.ReactClient/vite.config.ts
+++ Dev/Epm.FarmRoots.UI/Epm.FarmRoots.UI.ReactClient/vite.config.ts
@@ -7,6 +7,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build:{
+        rollupOptions:{
+            external:[
+                '@fortawesome/fontawesome-svg-core',
+            ]
+        }
+    },
     test: {
         // reporters: [
         //     ["vitest-sonar-reporter", { outputFile: "sonar-report.xml" }],
